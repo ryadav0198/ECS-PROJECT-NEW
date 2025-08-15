7c158fc9892753e6f0227657ca0da3343ae27a24
ryadav0198 <rohityadav0198@gmail.com>
2025-08-15 15:26:34 +0000

    this is new commit code

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from ECS Fargate via GitHub Actions! 🚀 Version 1 from rohit"
+    return "Hello from ECS Fargate via GitHub Actions! 🚀 Version 1.0.1 from rohit yadav"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
