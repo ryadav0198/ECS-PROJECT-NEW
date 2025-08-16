bcf99e85fc0692d91798f63d7084facb5f0a3ba7
ryadav0198 <rohityadav0198@gmail.com>
2025-08-16 11:50:54 +0000

    this is for blue green deployment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from ECS Fargate via GitHub Actions! 🚀 Version 3.0.2 from rohit kumar yadav Now i'm more advanced"
+    return "Hello from ECS Fargate via GitHub Actions! 🚀 Version 4.0.2 from rohit kumar yadav Now i'm more advanced this is Blue green deployment"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
